6e654dbbbc3c17fa9838818f025bd9ac1e8de4cd
Yang Yuanxiong <aymiracle7@gmail.com>
2022-11-21 18:48:45 +0800

    feat: 基本布局

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -78,7 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Row(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -102,6 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            const Text("123123123123123123123123123123123123123123123123123123123123123123123123"),
           ],
         ),
       ),
---
+++ note.md
@@ -0,0 +1,11 @@
+# flutter_practice
+笔记
+
+1.界面基本结构和布局容器
+Scaffold->appBar|body|floatingActionButton
+appbar->title
+body->Center->child->Row/column/stack/container->widget
+布局容器理解:
+row column 对应线性布局
+stack 对应帧布局/相对布局
+container 可以设置边框 内边距、阴影,还有约束条件  像是cardView+constraintLayout
